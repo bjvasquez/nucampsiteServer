77769397bbddab005ee1f4c36c577a0da6f81ef1
Brian Vasquez <bjvasquez@gmail.com>
2020-02-28 06:05:42 -0600

    CORS exercise

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '191331092187419',
+        clientSecret: 'd113e4de7b2cf3ccd29031d3c6179da4'
+    }
 }
